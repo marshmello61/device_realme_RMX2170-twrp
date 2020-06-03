df4d59e27557b55e4fc47b73088d23958a7f5b98
Nauval Rizky <enuma.alrizky@gmail.com>
2020-06-03 17:59:01 +0700

    RMX2061: post-boot: use Magisk's resetprop
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- recovery/root/sbin/init.recovery.post-boot.sh
+++ recovery/root/sbin/init.recovery.post-boot.sh
@@ -2,10 +2,10 @@
 realdevice=$(getprop ro.omni.device)
 
 # Fix decrypted Stock ROM.zip Flashing
-setprop ro.product.name $realdevice
-setprop ro.product.odm.name $realdevice
-setprop ro.product.system.name $realdevice
-setprop ro.product.vendor.name $realdevice
-setprop ro.product.product.device $realdevice
+resetprop ro.product.name $realdevice
+resetprop ro.product.odm.name $realdevice
+resetprop ro.product.system.name $realdevice
+resetprop ro.product.vendor.name $realdevice
+resetprop ro.product.product.device $realdevice
 
 exit 0

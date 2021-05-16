bbc0e0f69a120ebe2434746155ed3b5c18ced502
Mayur <ultramayur123@gmail.com>
2021-05-16 08:13:49 +0000

    RMX2170: export needed flags using vendorsetup.sh
    Signed-off-by: Mayur <ultramayur123@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,32 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export LC_ALL="C"
+
+export OF_FLASHLIGHT_ENABLE=1
+export OF_SUPPORT_OZIP_DECRYPTION=1
+
+export OF_MAINTAINER="marshmello_61"
+export FOX_R11=1
+export OF_SKIP_FBE_DECRYPTION=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+#export OF_KEEP_FORCED_ENCRYPTION=1
